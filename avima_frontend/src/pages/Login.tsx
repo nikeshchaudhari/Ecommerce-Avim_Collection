f468d408d8c4d79fe1d3c2434df32d007e50ce7f
nikesh <info.nikesh12@gmail.com>
2026-05-21 14:30:42 +0545

    avima

--- avima_frontend/src/pages/Login.tsx
+++ avima_frontend/src/pages/Login.tsx
@@ -1,12 +1,66 @@
 import { useState } from "react";
 import UserNavbar from "../components/UserNavbar";
 import { FiEye, FiEyeOff } from "react-icons/fi";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { useFormik } from "formik";
+import { LoginValid } from "../Schemas/LoginValid";
+import axios from "axios";
 
+import { toast } from "react-toastify";
 
 const Login = () => {
   const [passwordShow, setPasswordShow] = useState(false);
+  const navigate = useNavigate();
+  const {
+    values,
+    errors,
+    resetForm,
+    handleChange,
+    handleBlur,
+    handleSubmit,
+    touched,
+  } = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+    },
+    validationSchema: LoginValid,
+    onSubmit: async (values) => {
+      // console.log(values);
 
+      try {
+        const res = await axios.post(
+          "http://localhost:3000/users/login",
+          values,
+        );
+        console.log("userdata",res.data);
+
+        const data = res.data;
+
+        if (data.token) {
+          // const token = data.token;
+
+          localStorage.setItem("token", data.token);
+          localStorage.setItem("role", data.role);
+          localStorage.setItem("fullName", data.fullName);
+        }
+
+        toast.success("Login Sucessfully..");
+
+        if (data.role === "admin") {
+          navigate("/dashboard");
+        } else if (data.role === "user") {
+          navigate("/home");
+        } else {
+          navigate("/");
+        }
+
+        resetForm();
+      } catch (err: any) {
+        toast.error(err.res?.data?.msg || "Something went wrong");
+      }
+    },
+  });
   return (
     <>
       <main className="bg-[#faf5ec] w-full min-h-screen">
@@ -17,10 +71,12 @@ const Login = () => {
               AVIMA MEMBERS
             </span>
             <h1 className="font-cormorant text-[25px] md:text-4xl text-red-800 mb-5 md:mb-10">
-              Create account
+              Welcome back
             </h1>
-            <form className=" w-[80vw] md:w-fix lg:w-auto">
-          
+            <form
+              onSubmit={handleSubmit}
+              className=" w-[80vw] md:w-fix lg:w-auto"
+            >
               <div className="flex flex-col mb-5 ">
                 <label htmlFor="email" className=" font-inter">
                   Email <span className="text-red-500">*</span>{" "}
@@ -28,8 +84,15 @@ const Login = () => {
                 <input
                   type="email"
                   id="email"
+                  name="email"
+                  value={values.email}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
                   className="border border-black/20 py-2 shadow-lg rounded w-full md:w-fix lg:w-[25vw] outline-none px-2 focus:ring-1"
                 />
+                {touched.email && errors.email && (
+                  <p className="text-sm text-red-500">{errors.email}*</p>
+                )}
               </div>
               <div className="flex flex-col mb-5 relative">
                 <label htmlFor="password" className=" font-inter">
@@ -38,6 +101,10 @@ const Login = () => {
                 <input
                   type={passwordShow ? "text" : "password"}
                   id="password"
+                  name="password"
+                  value={values.password}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
                   className="border border-black/20 py-2 shadow-lg rounded md:w-fix lg:w-[25vw] outline-none px-2 pr-8 focus:ring-1"
                 />
                 <button
@@ -47,25 +114,38 @@ const Login = () => {
                 >
                   {passwordShow ? <FiEye size={20} /> : <FiEyeOff size={20} />}
                 </button>
+                {touched.password && errors.password && (
+                  <p className="text-sm text-red-500">{errors.password}*</p>
+                )}
               </div>
-              
 
               <button
-                type="button"
+                type="submit"
                 className="w-full bg-red-800 text-[20px] text-white py-2 cursor-pointer transition hover:bg-red-900 rounded"
               >
-                Create account
+                Login
               </button>
-             <div className="flex justify-between mt-4 ">
-               <h2 className="font-inter">Forgot password?</h2>
-               <h2 className="font-inter">New to AVIMA? <Link to="/register" className="text-red-800 font-inter"> Create account</Link> </h2>
-             </div>
+              <div className="flex justify-between mt-4 ">
+                <h2 className="font-inter text-[12px] md:text-[16px]">
+                  Forgot password?
+                </h2>
+                <h2 className="font-inter  text-[12px] md:text-[16px] ">
+                  New to AVIMA?{" "}
+                  <Link
+                    to="/register"
+                    className="text-red-800 font-inter  text-[12px] md:text-[16px]"
+                  >
+                    {" "}
+                    Create account
+                  </Link>{" "}
+                </h2>
+              </div>
             </form>
           </div>
         </section>
       </main>
     </>
   );
-}
+};
 
-export default Login
+export default Login;
