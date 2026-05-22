46a7ef2ae8ae9026503c9aeba0729b15598911d5
nikesh <info.nikesh12@gmail.com>
2026-05-22 15:13:28 +0545

    avima

--- avima_frontend/src/AdminDashboard/component/Navbar.tsx
+++ avima_frontend/src/AdminDashboard/component/Navbar.tsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react";
 import logo from "../../assets/avima-logo.png";
 import { RxHamburgerMenu } from "react-icons/rx";
+import { LiaSignOutAltSolid } from "react-icons/lia";
+import { LiaExternalLinkAltSolid } from "react-icons/lia";
 
 const Navbar = () => {
   const [dark, setDark] = useState(false);
@@ -16,7 +18,7 @@ const Navbar = () => {
       <nav className="px-4 py-3">
         <div className="flex items-center justify-between">
           <div className="flex  items-center  gap-5">
-            <RxHamburgerMenu size={25} />
+            <RxHamburgerMenu size={25} className="lg:hidden" />
 
             {/* Logo */}
             <div className="flex items-center gap-2">
@@ -37,7 +39,20 @@ const Navbar = () => {
           </div>
 
           {/* Right side (future menu/buttons) */}
-          <div className="flex items-center gap-4"></div>
+          <div className="flex items-center gap-4">
+            <div className="flex items-center gap-1 text-black/40 hover:text-black cursor-pointer text-[12px] md:text-[10px]">
+              <LiaExternalLinkAltSolid />
+            <span className="whitespace-nowrap">
+              {" "}
+              
+              View Site
+            </span>
+            </div>
+            <div className=" flex  items-center gap-1 text-white bg-red-700 px-2 md:px-2 py-1">
+              <LiaSignOutAltSolid size={20} />
+              <button type="button" className="text-[12px] md:text-[10px] whitespace-nowrap">Sign out</button>
+            </div>
+          </div>
         </div>
       </nav>
     </main>
