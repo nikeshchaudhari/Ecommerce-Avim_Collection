ac388548b09d067445aeb5567f90cd1908799ff5
nikesh <info.nikesh12@gmail.com>
2026-05-31 17:10:04 +0545

    avima

--- avima_frontend/src/AdminDashboard/pages/AdminProducts.tsx
+++ avima_frontend/src/AdminDashboard/pages/AdminProducts.tsx
@@ -2,10 +2,10 @@ import Navbar from "../component/Navbar";
 import AdminSideBar from "../component/AdminSideBar";
 import { IoSearchOutline } from "react-icons/io5";
 import { useEffect, useState } from "react";
+import { WiStars } from "react-icons/wi";
 
 const AdminProducts = () => {
-
-     const [open, setOpen] = useState(false);
+  const [open, setOpen] = useState(false);
 
   useEffect(() => {
     if (open) {
@@ -42,67 +42,65 @@ const AdminProducts = () => {
 
                 <p className="font-inter text-black/60">61 item(s)</p>
               </div>
-              
-                <div className="relative">
-                  <input
-                    type="text"
-                    placeholder="Search products...."
-                    className="border border-black/10 dark:border-white/40 mx-4 pl-10 pr-3  py-1.5 font-inter outline-none shadow "
-                  />
-                  <IoSearchOutline className="absolute top-3 left-8" />
-                  <button
-                    type="button"
-                    onClick={()=>setOpen(!open)}
-                    className="bg-yellow-500 px-6  py-1.5 rounded hover:bg-yellow-400 transition duration-300 font-inter"
-                  >
-                    + New product
-                  </button>
-                
+
+              <div className="relative">
+                <input
+                  type="text"
+                  placeholder="Search products...."
+                  className="border border-black/10 dark:border-white/40 mx-4 pl-10 pr-3  py-1.5 font-inter outline-none shadow "
+                />
+                <IoSearchOutline className="absolute top-3 left-8" />
+                <button
+                  type="button"
+                  onClick={() => setOpen(!open)}
+                  className="bg-yellow-500 px-6  py-1.5 rounded hover:bg-yellow-400 transition duration-300 font-inter"
+                >
+                  + New product
+                </button>
               </div>
             </div>
-              {/* added catalog  */}
-{open && (
-  <div className="fixed inset-0 bg-black/50 flex justify-center items-center">
+            {/* added catalog  */}
+            {open && (
+              <div className="fixed inset-0 bg-black/50 flex justify-center items-center">
+                <div className="bg-white w-1/2 p-5 rounded shadow-lg relative">
+                  {/* close button */}
+                  <button
+                    onClick={() => setOpen(false)}
+                    className="absolute top-2 right-2 text-black"
+                  >
+                    ✖
+                  </button>
+                  <div className="flex items-center gap-2  text-yellow-500 ">
+                    <WiStars />
+                    <span className=" tracking-[4px] text-[12px] font-inter text-yellow-500">
+                      CATALOG
+                    </span>
+                  </div>
 
-    <div className="bg-white w-1/2 p-5 rounded shadow-lg relative">
+                  <h2 className="text-[25px] font-cormorant mb-4 font-light">
+                    New product
+                  </h2>
 
-      {/* close button */}
-      <button
-        onClick={() => setOpen(false)}
-        className="absolute top-2 right-2 text-black"
-      >
-        ✖
-      </button>
+                  <input
+                    type="text"
+                    placeholder="Product name"
+                    className="border w-full p-2 mb-3"
+                  />
 
-      <h2 className="text-xl font-cormorant mb-4">New Product</h2>
+                  <input
+                    type="number"
+                    placeholder="Price"
+                    className="border w-full p-2 mb-3"
+                  />
 
-      <input
-        type="text"
-        placeholder="Product name"
-        className="border w-full p-2 mb-3"
-      />
-
-      <input
-        type="number"
-        placeholder="Price"
-        className="border w-full p-2 mb-3"
-      />
-
-      {/* <button className="bg-green-600 text-white px-4 py-2 rounded w-full">
+                  {/* <button className="bg-green-600 text-white px-4 py-2 rounded w-full">
         Save
       </button> */}
-
-    </div>
-
-  </div>
-)}
-
-
+                </div>
+              </div>
+            )}
           </section>
         </div>
-
-      
-
       </main>
     </>
   );
