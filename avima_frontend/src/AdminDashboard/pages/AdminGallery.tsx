c0cee573f0aa4618cc7de24100e27fde4bfd3d61
nikesh <info.nikesh12@gmail.com>
2026-06-07 15:04:56 +0545

    avima

---
+++ avima_frontend/src/AdminDashboard/pages/AdminGallery.tsx
@@ -0,0 +1,15 @@
+import Navbar from "../component/Navbar";
+
+const AdminGallery = () => {
+  return (
+    <>
+      <main>
+        <div className="sticky top-0 z-50">
+          <Navbar />
+        </div>
+      </main>
+    </>
+  );
+};
+
+export default AdminGallery;
