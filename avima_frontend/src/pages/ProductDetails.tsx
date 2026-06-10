bdda3f37c7c42e2e78db1ad0fc4561b0c3ac9bb1
nikesh <info.nikesh12@gmail.com>
2026-06-10 16:43:05 +0545

    avima

--- avima_frontend/src/pages/ProductDetails.tsx
+++ avima_frontend/src/pages/ProductDetails.tsx
@@ -14,7 +14,8 @@ interface Product {
 }
 const ProductDetails = () => {
   const { id } = useParams();
-  const [product, setProduct] = useState<Product | null>(null);
+  const [product, setProduct] = useState<any | null>(null);
+  const [selectPhoto, setSelectPhoto] = useState(0);
   const [quantity, setQuantity] = useState(1);
   useEffect(() => {
     const fetchProduct = async () => {
@@ -35,18 +36,18 @@ const ProductDetails = () => {
       </div>
     );
   }
+  const photos = JSON.parse(product?.photos || "[]");
+
   return (
     <>
       <main className="bg-[#faf5ec] min-h-screen pb-16">
         <UserNavbar />
 
         <div className="  flex justify-center">
-            
           <div className="w-[80vw] ">
-           {/* breadcrum */}
+            {/* breadcrum */}
             <div className="mt-10">
               <div className="text-sm text-gray-500 flex gap-2">
-               
                 <Link to="/shop" className="hover:text-black">
                   Shop
                 </Link>
@@ -54,16 +55,37 @@ const ProductDetails = () => {
                 <span className="text-black">{product.name}</span>
               </div>
             </div>
-            
+
             {/* products details */}
             <div>
-                <div>
-                    
+              <div>
+                <div className="mt-5 flex md:block  justify-center ">
+                  <img
+                    src={photos[selectPhoto]?.url}
+                    className="w-50 h-75 md:w-125 md:h-150 rounded-lg"
+                  />
                 </div>
-                <div></div>
+                <div className="flex gap-5 mt-5">
+                  <div className="flex gap-3 mt-4">
+                    {photos.map((photo: any, index: number) => (
+                      <img
+                        key={index}
+                        src={photo.url}
+                        onClick={() => setSelectPhoto(index)}
+                        className={`w-20 h-20 object-cover rounded-md cursor-pointer border ${
+                          selectPhoto === index
+                            ? "border-red-900 border-2"
+                            : "border-gray-300"
+                        }`}
+                      />
+                    ))}
+                  </div>
+                </div>
+              </div>
+
+              <div></div>
             </div>
           </div>
-          
         </div>
       </main>
     </>
