a8f28060ce7684de4a41da67c43bfba52df220da
nikesh <info.nikesh12@gmail.com>
2026-06-14 22:28:59 +0545

    avima

--- avima_frontend/src/pages/CheckOut.tsx
+++ avima_frontend/src/pages/CheckOut.tsx
@@ -9,9 +9,28 @@ const Checkout = () => {
   const isOpen = useSelector((state: RootState) => state.cartOpen.isOpen);
   const user = JSON.parse(localStorage.getItem("user") || "{}");
   const cart = JSON.parse(localStorage.getItem(`cart_${user.id}`) || "[]");
-  console.log(cart);
+  const [formData, setFormData] = useState({
+    customerName: user.fullName || "",
+    customerEmail: user.email || "",
+    customerPhone: user.phone || "",
+    shippingAddress: user.address || "",
+  });
 
-
+  const order = {
+    userId: user.id,
+    customerName: user.fullName,
+    customerEmail: user.email || "",
+    customerPhone: user.phone,
+    shippingAddress: user.address,
+    status: "pending",
+    items: JSON.stringify(cart), // IMPORTANT
+    total: cart.reduce(
+      (sum: number, item: any) => sum + item.price * item.quantity,
+      0,
+    ),
+    notes: "",
+    whatsapp_message: "",
+  };
 
   return (
     <>
@@ -21,14 +40,14 @@ const Checkout = () => {
         </div>
 
         {isOpen && (
-          <div className="px-4 sm:px-6 lg:px-12 py-16">
+          <div className="px-4 sm:px-6 lg:px-12 py-5">
             <div className="max-w-6xl mx-auto">
               {/* PAGE MAIN TITLE & SUBTITLE */}
-              <div className="mb-12">
-                <div className="font-serif text-[38px] text-[#1C1C1C] tracking-wide mb-1">
+              <div className="mb-5">
+                <div className="font-serif text-[38px] dark:text-white text-[#1C1C1C] tracking-wide mb-1">
                   Checkout
                 </div>
-                <div className="text-[15px] text-gray-500 font-normal">
+                <div className="text-[15px]  dark:text-white   text-gray-500 font-normal">
                   Review and send your order to our atelier on WhatsApp.
                 </div>
               </div>
@@ -39,14 +58,20 @@ const Checkout = () => {
                 <div className="lg:col-span-7 space-y-6">
                   {/* FULL NAME */}
                   <div className="flex flex-col gap-2">
-                    <label className="text-[14px] font-medium text-gray-800">
+                    <label className="text-[14px] font-medium  dark:text-white  text-gray-800">
                       Full name <span className="text-[#80162B]">*</span>
                     </label>
                     <input
                       type="text"
                       name="fullName"
-                      
-                      className="w-full bg-[#FAF8F5] border border-gray-300 h-11 px-4 text-[14px] focus:outline-none focus:border-gray-500 transition-colors disabled:opacity-60"
+                      value={formData.customerName}
+                      onChange={(e) =>
+                        setFormData({
+                          ...formData,
+                          customerName: e.target.value,
+                        })
+                      }
+                      className="w-full dark:text-white  dark:bg-black bg-[#FAF8F5] border border-gray-300 h-11 px-4 text-[14px] focus:outline-none focus:border-gray-500 transition-colors disabled:opacity-60"
                       placeholder="Your name"
                     />
                   </div>
@@ -54,27 +79,32 @@ const Checkout = () => {
                   {/* PHONE & CITY GRID ROW */}
                   <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
                     <div className="flex flex-col gap-2">
-                      <label className="text-[14px] font-medium text-gray-800">
+                      <label className="text-[14px] font-medium  dark:text-white text-gray-800">
                         Phone <span className="text-[#80162B]">*</span>
                       </label>
                       <input
                         type="text"
                         name="phone"
-                      
-                        className="w-full bg-[#FAF8F5] border border-gray-300 h-11 px-4 text-[14px] focus:outline-none focus:border-gray-500 transition-colors disabled:opacity-60"
+                        value={formData.customerPhone}
+                        onChange={(e) =>
+                          setFormData({
+                            ...formData,
+                            customerPhone: e.target.value,
+                          })
+                        }
+                        className="w-full  dark:text-white  dark:bg-black bg-[#FAF8F5] border border-gray-300 h-11 px-4 text-[14px] focus:outline-none focus:border-gray-500 transition-colors disabled:opacity-60"
                         placeholder="Mobile number"
                       />
                     </div>
 
                     <div className="flex flex-col gap-2">
-                      <label className="text-[14px] font-medium text-gray-800">
+                      <label className="text-[14px] font-medium  dark:text-white text-gray-800">
                         City
                       </label>
                       <input
                         type="text"
                         name="city"
-                       
-                        className="w-full bg-[#FAF8F5] border border-gray-300 h-11 px-4 text-[14px] focus:outline-none focus:border-gray-500 transition-colors disabled:opacity-60"
+                        className="w-full  dark:text-white  dark:bg-black bg-[#FAF8F5] border border-gray-300 h-11 px-4 text-[14px] focus:outline-none focus:border-gray-500 transition-colors disabled:opacity-60"
                         placeholder="e.g. Kathmandu"
                       />
                     </div>
@@ -82,28 +112,26 @@ const Checkout = () => {
 
                   {/* SHIPPING ADDRESS */}
                   <div className="flex flex-col gap-2">
-                    <label className="text-[14px] font-medium text-gray-800">
+                    <label className="text-[14px] font-medium  dark:text-white text-gray-800">
                       Shipping address
                     </label>
                     <textarea
                       name="shippingAddress"
-                    
                       rows={4}
-                      className="w-full bg-[#FAF8F5] border border-gray-300 p-4 text-[14px] focus:outline-none focus:border-gray-500 transition-colors resize-none disabled:opacity-60"
+                      className="w-full  dark:text-white  dark:bg-black bg-[#FAF8F5] border border-gray-300 p-4 text-[14px] focus:outline-none focus:border-gray-500 transition-colors resize-none disabled:opacity-60"
                       placeholder="Area, Street name, House No..."
                     />
                   </div>
 
                   {/* ORDER NOTES */}
                   <div className="flex flex-col gap-2">
-                    <label className="text-[14px] font-medium text-gray-800">
+                    <label className="text-[14px] font-medium  dark:text-white text-gray-800">
                       Order notes
                     </label>
                     <textarea
                       name="orderNotes"
-                     
                       rows={4}
-                      className="w-full bg-[#FAF8F5] border border-gray-300 p-4 text-[14px] focus:outline-none focus:border-gray-500 transition-colors resize-none disabled:opacity-60"
+                      className="w-full  dark:text-white  dark:bg-black bg-[#FAF8F5] border border-gray-300 p-4 text-[14px] focus:outline-none focus:border-gray-500 transition-colors resize-none disabled:opacity-60"
                       placeholder="Tailoring, fabric preferences..."
                     />
                   </div>
@@ -111,7 +139,6 @@ const Checkout = () => {
                   {/* SUBMISSION BUTTON MIXED WITH LOADING SPINNER ARCHITECTURE */}
                   <button
                     type="button"
-                    
                     className="w-full bg-[#1BD741] text-white font-sans text-[15px] font-semibold tracking-wide h-13 flex items-center justify-center gap-2 rounded cursor-pointer hover:bg-[#18c33a] transition-colors select-none mt-8 shadow-sm disabled:bg-gray-400 disabled:cursor-not-allowed"
                   >
                     {/* {isSubmitting ? (
@@ -146,19 +173,18 @@ const Checkout = () => {
                         <div>Confirm via WhatsApp</div>
                       </>
                     )} */}
-
                     Confirm via WhatsApp
                   </button>
                 </div>
 
                 {/* RIGHT PANEL: ORDER SUMMARY */}
-                <div className="lg:col-span-5 bg-white border border-gray-200/60 p-8 shadow-[0_4px_24px_rgba(0,0,0,0.01)]">
-                  <div className="font-serif text-[22px] text-gray-900 tracking-wide mb-8">
+                <div className="lg:col-span-4  dark:bg-black bg-white border border-gray-200/60 p-8 shadow-[0_4px_24px_rgba(0,0,0,0.01)]">
+                  <div className="font-serif text-[22px] text-gray-900 tracking-wide mb-5">
                     Your Order
                   </div>
 
                   {/* ITEMS BRIEF SPECIFICATIONS LAYER */}
-                  <div className="space-y-4 max-h-[220px] overflow-y-auto pr-1 mb-6">
+                  <div className="space-y-4 max-h-55 overflow-y-auto pr-1 mb-6">
                     {cartItems.length === 0 ? (
                       <div className="text-gray-400 font-serif text-sm">
                         No items in cart
@@ -194,16 +220,12 @@ const Checkout = () => {
                   <div className="space-y-5 pb-6 border-b border-gray-200 pt-4 border-t border-gray-100">
                     <div className="flex justify-between items-center text-[14px]">
                       <div className="text-gray-500">Subtotal</div>
-                      <div className="font-medium text-gray-900">
-                        NPR 500
-                      </div>
+                      <div className="font-medium text-gray-900">NPR 500</div>
                     </div>
 
                     <div className="flex justify-between items-center text-[14px]">
                       <div className="text-gray-500">VAT (13%)</div>
-                      <div className="font-medium text-gray-900">
-                        NPR 5000
-                      </div>
+                      <div className="font-medium text-gray-900">NPR 5000</div>
                     </div>
                   </div>
 
